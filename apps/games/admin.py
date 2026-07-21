a9cf94c270a33b93ca76ba4ec2bb0e71bb833ec2
Vicenzo-Az <vicenzo.net@gmail.com>
2026-07-21 20:15:19 -0300

    feat: register game and user models in Django admin

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.games.infra.models import Game, Genre, LibraryEntry
+
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(LibraryEntry)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.users.infra.models import User
+
+admin.site.register(User)
