26a2fc46f0a66f32374b5864c30db1c135e68d85
Vicenzo-Az <vicenzo.net@gmail.com>
2026-07-23 02:53:28 -0300

    feat: implement Django user repository

--- apps/users/infra/repositories.py
+++ apps/users/infra/repositories.py
@@ -8,3 +8,23 @@ A implementar:
 Este é o adapter entre o ORM do Django (models.py) e os use cases, que não
 devem conhecer o ORM diretamente.
 """
+from apps.users.domain.entities import User as DomainUser
+from apps.users.infra.models import User as DjangoUser
+
+
+class DjangoUserRepository:
+    def is_email_registered(self, email: str) -> bool:
+        return DjangoUser.objects.filter(email=email).exists()
+
+    def create(self, email: str, name: str, password: str) -> DomainUser:
+        django_user = DjangoUser.objects.create_user(
+            email=email,
+            name=name,
+            password=password
+        )
+        return DomainUser(
+            id=django_user.id,
+            email=django_user.email,
+            name=django_user.name,
+            created_at=django_user.created_at,
+        )
